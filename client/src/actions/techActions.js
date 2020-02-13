e11b495e27eea6472ae2930a10e4078138b4652f
jesse10930 <ronda.jesse@gmail.com>
2020-02-13 11:13:39 -0500

    fixing broken nested folder

---
+++ client/src/actions/techActions.js
@@ -0,0 +1,85 @@
+import {
+  GET_TECHS,
+  ADD_TECH,
+  DELETE_TECH,
+  SET_LOADING,
+  TECHS_ERROR
+} from './types';
+
+// Gets techs from server
+export const getTechs = () => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('api/techs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_TECHS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response
+    });
+  }
+};
+//
+
+// Add tech to server
+export const addTech = tech => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('api/techs', {
+      method: 'POST',
+      body: JSON.stringify(tech),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_TECH,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response
+    });
+  }
+};
+//
+
+// Delete tech from server
+export const deleteTech = id => async dispatch => {
+  try {
+    setLoading();
+
+    await fetch(`api/techs/${id}`, {
+      method: 'DELETE'
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response
+    });
+  }
+};
+//
+
+// Set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING
+  };
+};
+//
---
+++ client/src/components/layouts/SearchBar.js
@@ -0,0 +1,59 @@
+import React, { useRef } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { searchLogs, getLogs } from '../../actions/logActions';
+
+const SearchBar = ({ searchLogs, getLogs }) => {
+  const text = useRef('');
+
+  const onClick = e => {
+    text.current.value = '';
+    getLogs();
+  };
+
+  const onKeyUp = async e => {
+    if (e.keyCode === 8) {
+      if (text.current.value !== '') {
+        getLogs();
+      }
+      let temp = e.target.value;
+      await getLogs();
+      searchLogs(temp);
+    } else if (text.current.value !== '') {
+      searchLogs(e.target.value);
+    } else {
+      getLogs();
+    }
+  };
+
+  return (
+    <nav style={{ marginBottom: '30px' }} className='blue'>
+      <div className='nav-wrapper'>
+        <form>
+          <div className='input-field'>
+            <input
+              id='search'
+              type='search'
+              placeholder='Search Logs...'
+              ref={text}
+              onKeyUp={onKeyUp}
+            />
+            <label className='label-icon' htmlFor='search'>
+              <i className='material-icons'>search</i>
+            </label>
+            <i className='material-icons' onClick={onClick}>
+              close
+            </i>
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
+};
+
+SearchBar.propTypes = {
+  searchLogs: PropTypes.func.isRequired,
+  getLogs: PropTypes.func.isRequired
+};
+
+export default connect(null, { searchLogs, getLogs })(SearchBar);
